bdbf2a7f5a43a573d9177e284ca6d5748a517f6f
MauricioMontes <00042823@uca.edu.sv>
2024-08-15 10:47:37 -0600

    mensaje sin firmar

--- test.cc
+++ test.cc
@@ -1,3 +1,6 @@
+//Por lo que veo, cada vez que quiero firmar algo con mi ssh, tengo que usar:
+//  git remote set-url origin git@github.com:MauricioMontes-UCA/[nombre-repositorio]
+
 #include <iostream>
 
 using namespace std;
